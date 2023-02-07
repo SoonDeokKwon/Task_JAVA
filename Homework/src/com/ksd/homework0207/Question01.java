c55756a92e19034c07b9ec8101ab14fcaa4c7846
admin <admin@YD05-19>
2023-02-07 17:50:41 +0900

    homework

---
+++ Homework/src/com/ksd/homework0207/Question01.java
@@ -0,0 +1,12 @@
+package com.ksd.homework0207;
+
+public class Question01 {
+	public static void main(String[] args) {
+		//1) 세 개의 단어를 입력 받아 그 중 가장 짧은 단어와 길이 리턴하기
+		//예시 )  입력> tiger
+		//입력> lion
+		//입력> rabbit
+		//출력> 가장 짧은 단어는 lion이며, 길이는 4 입니다
+		
+	}
+}
